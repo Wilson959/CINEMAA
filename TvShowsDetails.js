aa605fd1a0df37f544719380d01c743cdeac74b7
Wilson959 <150672235+Wilson959@users.noreply.github.com>
2024-08-27 15:36:16 +0530

    Update TvShowsDetails.js

--- TvShowsDetails.js
+++ TvShowsDetails.js
@@ -168,7 +168,7 @@ const Castfun = (castee) => {
 };
 
 const Trailerfunc = function (id) {
-  return `<iframe class="youtubePlayer" src="https://vidsrc.me/embed/tv/${id}" width="100%" height="100%" loading="lazy"  frameborder="0" allowfullscreen></iframe>`;
+  return `<iframe class="youtubePlayer" src="https://vidlink.pro/embed/tv/${id}" width="100%" height="100%" loading="lazy"  frameborder="0" allowfullscreen></iframe>`;
 };
 
 /*  CLCIKED*/
