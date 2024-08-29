4ecdadf4e93d1a7c3afd7fcb034fd605c71ba3ea
Wilson959 <150672235+Wilson959@users.noreply.github.com>
2024-08-29 21:49:18 +0530

    Update TvShowsDetails.js

--- TvShowsDetails.js
+++ TvShowsDetails.js
@@ -168,7 +168,7 @@ const Castfun = (castee) => {
 };
 
 const Trailerfunc = function (id) {
-  return `<iframe class="youtubePlayer" src="https://api.fmoviez.online/embed/tv/${id}/1/1" width="100%" height="100%" loading="lazy"  frameborder="0" allowfullscreen></iframe>`;
+  return `<iframe class="youtubePlayer" src="https://api.fmoviez.online/embed/tv/{id}/{season}/{episode}" width="100%" height="100%" loading="lazy"  frameborder="0" allowfullscreen></iframe>`;
 };
 
 /*  CLCIKED*/
