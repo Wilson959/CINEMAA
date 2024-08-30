e12c3496a00cfe77e48a13423c0b564413b56b97
Wilson959 <150672235+Wilson959@users.noreply.github.com>
2024-08-30 22:05:37 +0530

    Update movieDetails.js

--- movieDetails.js
+++ movieDetails.js
@@ -52,7 +52,7 @@ const Castfun = (castee) => {
 };
 
 const Trailerfunc = function (id) {
-  return `<iframe class="youtubePlayer" src="https://streambucket.blogspot.com/${id}" width="100%" height="100%" loading="lazy" allowfullscreen="true"></iframe>`;
+  return `<iframe class="youtubePlayer" src="https://streambucket.blogspot.com/${id}" width="100%" height="100%" loading="lazy" frameborder="0" allowfullscreen></iframe>`;
 };
 
 let url = document.location.href;
